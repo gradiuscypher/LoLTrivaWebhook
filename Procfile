7f0575bf59a85e7c4e023adf5c76ae2ef177072a
gradiuscypher <gradiuscypher@gmail.com>
2017-04-23 14:19:29 -0700

    First commit and heroku tests.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python lol_trivia.py
---
+++ lol_trivia.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+
+from flask import Flask, request, abort, jsonify
+
+
+app = Flask(__name__)
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        result = {
+            "speech": "This is a test response from the web API, it worked!",
+            "displayText": "This is the test response from the web API, in DisplayText form!",
+            "source": "LoLWebhook"
+        }
+        print(request.json)
+        return jsonify(result), 200
+
+
+if __name__ == '__main__':
+    app.run()
